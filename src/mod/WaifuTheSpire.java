1ac7c192d52fb8ca2ef489ae4c38613b62097ea2
Jedi515 <jedimailpost515@mail.ru>
2019-03-23 23:13:49 +1000

    Templated waifufying

--- src/mod/WaifuTheSpire.java
+++ src/mod/WaifuTheSpire.java
@@ -1,8 +1,19 @@
 package mod;
 
 import basemod.BaseMod;
+import basemod.ReflectionHacks;
 import basemod.interfaces.ISubscriber;
+import com.badlogic.gdx.math.MathUtils;
+import com.esotericsoftware.spine.AnimationState;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.Hitbox;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import mod.waifuthespire.util.TextureLoader;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 @SpireInitializer
 public class WaifuTheSpire
@@ -11,5 +22,42 @@ public class WaifuTheSpire
     public static void initialize()
     {
         BaseMod.subscribe(new WaifuTheSpire());
+    }
+
+    public static void waifufy(AbstractMonster waifuTarget, String imagePath, int imageWidth, int imageHeight)
+    {
+        try {
+            ReflectionHacks.setPrivate(waifuTarget, AbstractCreature.class, "atlas", null);
+            ReflectionHacks.setPrivate(waifuTarget, AbstractMonster.class, "img", TextureLoader.getTexture(imagePath));
+            float hb_w = imageWidth * Settings.scale;
+            float hb_h = imageHeight * Settings.scale;
+            waifuTarget.hb_w = hb_w;
+            waifuTarget.hb_h = hb_h;
+            Hitbox hb = waifuTarget.hb;
+            hb.width = hb_w;
+            hb.height = hb_h;
+            hb.cX = hb.x + hb.width / 2.0F;
+            hb.cY = hb.y + hb.height / 2.0F;
+            Method refreshHbLoc = AbstractCreature.class.getDeclaredMethod("refreshHitboxLocation");
+            refreshHbLoc.setAccessible(true);
+            refreshHbLoc.invoke(waifuTarget);
+            waifuTarget.refreshIntentHbLocation();
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void waifufy(AbstractMonster waifuTarget, String atlasPath, String jsonPath)
+    {
+        try {
+        Method loadAnimationMethod = AbstractCreature.class.getDeclaredMethod("loadAnimation",String.class, String.class, float.class);
+        loadAnimationMethod.setAccessible(true);
+        loadAnimationMethod.invoke(waifuTarget,atlasPath, jsonPath, 1.0F);
+        AnimationState.TrackEntry e = waifuTarget.state.setAnimation(0, "Idle", true);
+        e.setTime(e.getEndTime() * MathUtils.random());
+        e.setTimeScale(MathUtils.random(0.7F, 1.0F));
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/mod/waifuthespire/patches/FungiBeastPatch.java
+++ src/mod/waifuthespire/patches/FungiBeastPatch.java
@@ -1,18 +1,9 @@
 package mod.waifuthespire.patches;
 
-import basemod.ReflectionHacks;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.helpers.Hitbox;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.monsters.exordium.FungiBeast;
-import mod.waifuthespire.util.TextureLoader;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import static mod.WaifuTheSpire.waifufy;
 
 public class FungiBeastPatch
 {
@@ -21,32 +12,7 @@ public class FungiBeastPatch
     {
         public static void Postfix(FungiBeast __instance, float x, float y)
         {
-            try {
-//                Method loadAnimationMethod = AbstractCreature.class.getDeclaredMethod("loadAnimation",String.class, String.class, float.class);
-//                loadAnimationMethod.setAccessible(true);
-//                loadAnimationMethod.invoke(__instance,"resources/monsters/FungiBeast/FungiBeast.atlas", "resources/monsters/FungiBeast/FungiBeast.json", 1.0F);
-//                AnimationState.TrackEntry e = __instance.state.setAnimation(0, "Idle", true);
-//                e.setTime(e.getEndTime() * MathUtils.random());
-//                e.setTimeScale(MathUtils.random(0.7F, 1.0F));
-
-                ReflectionHacks.setPrivate(__instance, AbstractCreature.class, "atlas", null);
-                ReflectionHacks.setPrivate(__instance, AbstractMonster.class, "img", TextureLoader.getTexture("resources/monsters/FungiBeast/FungiBeast.png"));
-                float hb_w = 196 * Settings.scale;
-                float hb_h = 281 * Settings.scale;
-                __instance.hb_w = hb_w;
-                __instance.hb_h = hb_h;
-                Hitbox hb = __instance.hb;
-                hb.width = hb_w;
-                hb.height = hb_h;
-                hb.cX = hb.x + hb.width / 2.0F;
-                hb.cY = hb.y + hb.height / 2.0F;
-                Method refreshHbLoc = AbstractCreature.class.getDeclaredMethod("refreshHitboxLocation");
-                refreshHbLoc.setAccessible(true);
-                refreshHbLoc.invoke(__instance);
-                __instance.refreshIntentHbLocation();
-            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                e.printStackTrace();
-            }
+            waifufy(__instance, "resources/monsters/FungiBeast/FungiBeast.png", 196, 281);
         }
     }
 }
